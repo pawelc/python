3355d11d6643cc2240531a165704de831b024656
pawelc <pawelc@gmail.com>
2015-12-19 18:55:10 +0000

    changes

---
+++ sandbox/autograd/logisticRegression.py
@@ -0,0 +1,42 @@
+import autograd.numpy as np
+from autograd import grad
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 0.5*(np.tanh(x) + 1)
+
+def logistic_predictions(weights, inputs):
+    # Outputs probability of a label being true according to logistic model.
+    return sigmoid(np.dot(inputs, weights))
+
+def training_loss(weights):
+    # Training loss is the negative log-likelihood of the training labels.
+    preds = logistic_predictions(weights, inputs)
+    label_probabilities = preds * targets + (1 - preds) * (1 - targets)
+    return -np.sum(np.log(label_probabilities))
+
+# plot logistic
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# x=np.linspace(-2, 2, 100);
+# ax.plot(x, sigmoid(x))
+# plt.show()
+
+# Build a toy dataset.
+inputs = np.array([[0.52, 1.12,  0.77],
+                   [0.88, -1.08, 0.15],
+                   [0.52, 0.06, -1.30],
+                   [0.74, -2.49, 1.39]])
+targets = np.array([True, True, False, True])
+
+# Define a function that returns gradients of training loss using autograd.
+training_gradient_fun = grad(training_loss)
+
+# Optimize weights using gradient descent.
+weights = np.array([0.0, 0.0, 0.0])
+print "Initial loss:", training_loss(weights)
+for i in xrange(1000000):
+    weights -= training_gradient_fun(weights) * 0.01
+    print "Loss:", training_loss(weights)
+
+print  "Trained loss:", training_loss(weights)
