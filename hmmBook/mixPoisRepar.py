b8a1e830f3a998ab7e2bbdc9f6766a30a83ef5dc
Pawel Chilinski <pawelc@gmail.com>
2015-12-20 09:22:18 +0000

    com

--- hmmBook/mixPoisRepar.py
+++ hmmBook/mixPoisRepar.py
@@ -1,17 +1,29 @@
 #mixture of gaussian reparametrization
 import numpy as np
-
-def mixPoissonLogLikelihood(x,lambdaParams,dpois,mixingParams):
-    np.sum(np.log(np.outer(x,lambdaParams,dpois)*mixingParams))
-
-
-with open("../data/earthquakes.txt", "r") as infile:
-    data = [int(line.split()[1]) for line in infile]
-
+from scipy.stats import poisson
+from autograd import grad
 
 lambdaParams = lambda eta:np.exp(eta)
 mixingParams2_n = lambda tau:np.exp(tau)/(1+np.sum(np.exp(tau)))
 mixingParams = lambda tau: [1-sum(mixingParams2_n(tau))] + mixingParams2_n(tau);
 
+with open("../data/earthquakes.txt", "r") as infile:
+    data = [int(line.split()[1]) for line in infile]
 
-print mixPoissonLogLikelihood(data, lambdaParams, mixingParams)
+def mixPoissonLogLikelihood(lambdaParams,mixingParams):
+    np.sum(np.log(np.inner(poisson.pmf(np.outer(data,lambdaParams)),mixingParams)))
+
+def mixPoissonLogLikelihoodUnconstraint(eta,tau):
+    mixPoissonLogLikelihood(lambdaParams(eta),mixingParams(tau))
+
+eta=np.array([0,0])
+tau=np.array([0,0])
+
+training_gradient_fun = grad(mixPoissonLogLikelihoodUnconstraint)
+
+print "Initial loglikelihood:", mixPoissonLogLikelihoodUnconstraint(eta,tau)
+for i in xrange(1000000):
+    deta,dtau = training_gradient_fun(eta,tau)
+    eta+=deta * 0.01
+    tau+=dtau * 0.01
+    print "loglikelihood:", mixPoissonLogLikelihoodUnconstraint(eta,tau)
